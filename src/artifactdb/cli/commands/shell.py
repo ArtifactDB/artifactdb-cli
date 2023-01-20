7c9591b798e49969de29348d9ee1509bc0f06cef
Sebastien Lelong <lelongs@gene.com>
2023-01-20 23:44:04 +0000

    fix click-shell breaks line feed when accepting default answers from Prompt/Confirm

--- src/artifactdb/cli/commands/shell.py
+++ src/artifactdb/cli/commands/shell.py
@@ -1,7 +1,6 @@
 from typer import Typer, Context
 from rich import print, print_json
 from rich.console import Console
-from click_shell import make_click_shell
 
 from ..cliutils import get_historyfile_path, load_current_context
 
@@ -25,11 +24,14 @@ def shell_command(ctx: Context):
     """
     Launch interactive shell
     """
+    # post-pone import until we known we need a shell. click-shell conflicts
+    # with typer.Prompt when no value entered (accepting the default), no line feed.
+    # I wasn't able to fix the issue when using shell+prompt, but at least, shell
+    # doesn't pollute the CLI when not using it
+    from click_shell import make_click_shell
     print(":classical_building:  Welcome to the [bright_black]ArtifactDB shell[/bright_black], type `help` for available commands")
     adbctx = load_current_context()
     print(f"Active context {adbctx['name']!r}: [blue3]{adbctx['url']}[/blue3]")
-
-
     shell = make_click_shell(
         # ctx.parent represents (I think) the context containing the adb CLI and all the commands
         # which is what click-shell wants (multi-commands context)

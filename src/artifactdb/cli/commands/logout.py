9881b29f1a5d47c69da327220537ebe60958adca
Sebastien Lelong <lelong.sebastien@gene.com>
2023-02-16 15:53:45 -0800

    option to remove cached credentials in logout command

--- src/artifactdb/cli/commands/logout.py
+++ src/artifactdb/cli/commands/logout.py
@@ -16,6 +16,7 @@ from ..cliutils import (
     PermissionsInfo,
     InvalidArgument,
     load_current_context,
+    get_contextual_client,
     save_context,
 )
 
@@ -25,10 +26,20 @@ COMMAND_FUNC = "logout_command"
 app = Typer()
 
 
-def logout_command():
+def logout_command(
+    purge: bool = Option(
+        False,
+        help="Logout and delete cached credentials. Subsequent login will trigger authentication again."
+    ),
+        ):
     """
     Switch to anonymous access. Only effective within ArtifactDB shell.
     """
+    if purge:
+        client = get_contextual_client()
+        if client._auth:
+            print(":broom: Removing cached credentials")
+            client._auth._cache_file.unlink()
     ctx = load_current_context()
     ctx["auth"]["anonymous"] = True
     save_context(ctx["name"], ctx, overwrite=True, quiet=True)
